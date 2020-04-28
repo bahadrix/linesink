83dcc2ffaed7115ea151cf9138b9e38b7956e04f
Bahadir <bahadrix@gmail.com>
2020-04-28 17:53:05 +0300

    documentation

--- cmd/nats.go
+++ cmd/nats.go
@@ -9,13 +9,8 @@ import (
 // natsCmd represents the nats command
 var natsCmd = &cobra.Command{
 	Use:   "nats",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "NATS sink",
+	Long:  `Publish lines to NATS endpoint`,
 	Run: func(cmd *cobra.Command, args []string) {
 		url, _ := cmd.Flags().GetString("url")
 		subject, _ := cmd.Flags().GetString("subject")
@@ -48,14 +43,4 @@ func init() {
 
 	_ = natsCmd.MarkFlagRequired("url")
 	_ = natsCmd.MarkFlagRequired("subject")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// natsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// natsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -17,16 +17,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "linesink",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Piped lines processor",
+	Long:  ``,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -85,7 +77,6 @@ func readPipedLines(channelSize int) (lineChannel chan []byte, err error) {
 	if err != nil {
 		return
 	}
-
 
 	reader := bufio.NewReader(os.Stdin)
 	lineChannel = make(chan []byte, channelSize)
